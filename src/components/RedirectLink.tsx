61377d9d987fd4cc6f23aa29386e5073f0fb706b
litox06 <litox06@gmail.com>
2025-04-17 18:48:51 -0400

    Added modal to request creation of user (Director or MINERD) via Login component.

--- src/components/RedirectLink.tsx
+++ src/components/RedirectLink.tsx
@@ -2,12 +2,18 @@ import React from 'react'
 
 interface RedirectLinkProps {
   text: string
-  href: string
+  href?: string
   ariaLabel?: string
+  onClick?: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void
 }
 
-const RedirectLink: React.FC<RedirectLinkProps> = ({ text, href, ariaLabel }) => (
-  <a href={href} aria-label={ariaLabel ?? text} className="text-sm text-[#3498db] hover:underline">
+const RedirectLink: React.FC<RedirectLinkProps> = ({ text, href = '#', ariaLabel, onClick }) => (
+  <a
+    href={href}
+    onClick={onClick}
+    aria-label={ariaLabel ?? text}
+    className="text-sm text-[#3498db] hover:underline cursor-pointer"
+  >
     {text}
   </a>
 )
